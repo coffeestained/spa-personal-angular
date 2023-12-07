18e16de276c6e90b67665a9783612702259043f1
Matthew Grady <matthewbgrady90@gmail.com>
2023-12-06 22:21:29 -0500

    Snapshot

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,5 +1,4 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
-const ArcGISPlugin = require("@arcgis/webpack-plugin");
 
 const config = withModuleFederationPlugin({
   name: 'maps',
@@ -10,27 +9,6 @@ const config = withModuleFederationPlugin({
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
 });
-
-// Prior to this addition build size was 29mb (uncompressed)
-// After this addition, build size 2mb (uncompressed);
-config.plugins.push(
-  new ArcGISPlugin({
-    locales: ['en'],
-    userDefinedExcludes: [
-      "@arcgis/core/layers/BingMapsLayer",
-      "@arcgis/core/layers/CSVLayer",
-      "@arcgis/core/layers/GeoRSSLayer",
-      "@arcgis/core/layers/ImageryLayer",
-      "@arcgis/core/layers/KMLLayer",
-      "@arcgis/core/layers/MapImageLayer",
-      "@arcgis/core/layers/OpenStreetMapLayer",
-      "@arcgis/core/layers/StreamLayer",
-      "@arcgis/core/layers/WMSLayer",
-      "@arcgis/core/layers/WMTSLayer",
-      "@arcgis/core/layers/WebTileLayer"
-    ]
-  })
-);
 
 module.exports = config;
 
